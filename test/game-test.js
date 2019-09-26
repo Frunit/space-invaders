5ef20d1739b938c821f59fd34537f87c75e6d981
Frunit <frunit@frunit.de>
2019-09-26 15:42:29 +0200

    Added some tests for Game

--- test/game-test.js
+++ test/game-test.js
@@ -3,20 +3,20 @@
 import {Game} from '../game.js';
 import {options, levels} from '../options.js';
 
-// TODO: Test if options exist.
-// TODO: Test sending "Enter" (or other keys) to input and check the response. This also triggers a stage change from start to game!
-// TODO: Test for the presence of screen
+// TODO: Game test input
 // TODO: Test multiple stage changes
 
 
 QUnit.test('Game starting', function(assert) {
 	const game = new Game(options, levels);
-
-	assert.ok(typeof resources !== 'undefined', 'Global variable resources exists');
-
-	game.start();
+	// game.start() will be automatically called by resources
 	// During testing, game.loop() will not request itself, so it is no actual loop!
 
+	assert.ok(typeof resources === 'object', 'Global variable resources exists');
+	assert.ok(game.stage !== null, 'There is a stage');
+	assert.ok(game.screen !== null, 'There is a screen');
+	assert.strictEqual(game.stage.name, 'start', 'Right stage loaded (start)');
+
 	let result = game.update();
-	assert.ok(result === null, 'update returns null');
+	assert.ok(result === null, 'update returns null (no new stage)');
 });
